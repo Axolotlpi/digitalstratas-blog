2323baf276e6241468ed956b95511dafb634cc6f
Axolotlpi <cfc3.1415@gmail.com>
2025-07-28 22:06:11 -0500

    wipe uneeded content to start fresh

--- src/consts.ts
+++ src/consts.ts
@@ -1,21 +1,21 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
+  NAME: "Digital Stratas",
+  EMAIL: "axolotltau@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 4,
+  NUM_WORKS_ON_HOMEPAGE: 0,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Digital Stratas makes sure web tech makes life eaiser, not harder",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION: "A collection of tips and suggestions on how to use new and old technologes. Coming from people who use them.",
 };
 
 export const WORK: Metadata = {
@@ -25,20 +25,20 @@ export const WORK: Metadata = {
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION: "A sample of projects that may help you, and show what we can do for you.",
 };
 
 export const SOCIALS: Socials = [
   { 
     NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    HREF: "https://twitter.com/axolotltau",
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/axolotlpi"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/caleb-favela",
   }
 ];
